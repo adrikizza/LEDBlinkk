e352ce3d04901fbf4130d3a95007dba17529030a
adrikizza <adrikizza01tkj4@gmail.com>
2016-09-21 14:03:05 +0700

    Add files via upload

---
+++ sketch_sep21a.ino
@@ -0,0 +1,11 @@
+void setup() {
+  pinMode(2, OUTPUT); // menginisialisasi digital pin 2 sebagai output.
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(2, HIGH);   // Mengatur LED agar Menyala.
+  delay(1500);              // Tunggu sampai 1,5 detik.
+  digitalWrite(2, LOW);    // Mengatur LED agar Mati.
+  delay(1500);              // Tunggu sampai 1,5 detik.
+}
